3b5a58e980894d0ba9311e32d647e8eefd8f5bdd
bheemdevhub <devhub@bheemverse.com>
2025-07-29 16:09:39 +0530

    chore: add and update setup.py for editable install

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='bheem-project_management',
     version='1.0.0',
-    packages=find_packages(),
-    install_requires=[], # Add module-specific dependencies
+    packages=find_packages(include=["project_management", "project_management.*"]),
+    install_requires=[],
     include_package_data=True,
     description='Bheem Project_management ERP module',
     author='Bheem Core Team',
